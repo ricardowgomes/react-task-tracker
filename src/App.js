ba8cb5c4d886df7fb47fe137348a14c063b5c328
Ricardo Wagner Gomes <ricardowgomes@outlook.com>
2021-08-01 15:20:37 -0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Header from './components/Header'
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello From React</h1>
-      <h2>Hello</h2>
+    <div className="container">
+      <Header title="Hello" />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = (props) => {
+  return (
+    <header>
+      <h1>Task Tracker {props.title}</h1>
+    </header>
+  )
+}
+
+export default Header
